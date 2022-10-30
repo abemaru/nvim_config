667ada40a5dd23ee637966e32cb818856a0f78a8
abemaru <ch4nge.lightening@gmail.com>
2022-10-30 21:53:12 +0900

    add auto closing brackets

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 require("base")
+require("mapping")
 require("options")
 require("plugins")
 require("color")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,7 +86,7 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 require("mason-lspconfig").setup_handlers({ function(server)
 	local opt = {
-		capabilities = require("cmp_nvim_lsp").update_capabilities(
+		capabilities = require("cmp_nvim_lsp").default_capabilities(
 			vim.lsp.protocol.make_client_capabilities()
 		)
 	}
